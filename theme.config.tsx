77c7cbf63e44c768e1efa5d480f7fb0b11f702f2
valsod <valdeci.barbosa@olos.com.br>
2023-01-11 11:02:25 -0300

    change description and start integration

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Agent SDK Docs</span>,
+  logo: <span>Olos Agent SDK Docs</span>,
   head: (
     <>
       <title>Olos Agent SDK Docs</title>
