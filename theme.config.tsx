8cefa29bfff9893c1714e84c7b3e7be5d6778a89
valsod <valdeci.barbosa@olos.com.br>
2023-01-16 18:41:05 -0300

    add section by api

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Olos Agent SDK Docs</span>,
+  logo: <span>Olos para Desenvolvedores</span>,
   head: (
     <>
-      <title>Olos Agent SDK Docs</title>
+      <title>Olos para Desenvolvedores</title>
       <link
         rel="icon"
         href="https://www.olos.com.br/wp-content/uploads/2020/03/cropped-android-icon-192x192-1-32x32.png"
